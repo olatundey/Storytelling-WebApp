b6f2851d7156d58de6fbe2f4df211f04ad32eeec
Olatunde Yusuf <123118889+olatundey@users.noreply.github.com>
2023-03-12 13:14:49 +0000

    str

--- editstory.php
+++ editstory.php
@@ -28,38 +28,73 @@ $conn->close();
 // Display HTML form pre-populated with existing story data
 ?>
 <!DOCTYPE html>
-<html>
-<head>
-	<title>Edit Story</title>
-</head>
-<body>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0 minimum-scale=1, maximum-scale=1">
+        <title>Tourview</title>
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="unsemantic-grid-responsive-tablet.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap-theme.min.css">
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB3oeQSbb4oNhgeNqTBrdMlqyFSPD9Hg7s"></script>
+    </head>
+
+    <body>
+        <header class="container">
+            <div class="col-md-12">
+                <div id="headerContainer" class="row">
+                    <div class="col-md-2">
+                        <p>Tourview</p>
+                    </div>
+                
+                    <div class="col-md-10">
+                    <nav>
+                        <ul class="nav justify-content-end">
+                            <li><a href = "index.html" class="nav-item active" >Home</a></li>
+                            <li><a href = "stories.php" class="nav-item">Stories</a></li>
+                            <li><a href = "about.php" class="nav-item">About Us</a></li>
+                            <li><a href = "storytelleruser.php" class="nav-item">My Account</a></li>   
+                            <li><a href = "logout.php" class="nav-item">Logout</a></li>   
+                        </ul>
+                    </nav>
+                    </div>
+                </div>    
+            </div>
+
+        </header>
+
+        <main>
+        <p>Welcome StoryTeller, <?php print $user; ?>!</p>
 	<h1>Edit Your Story</h1>
 	<form method="post" action="updatestory.php">
-		<label for="name">Name:</label>
-		<input type="text" id="name" name="name" value="<?php echo $story['name']; ?>"><br>
+		<label for="name">Userame:</label>
+		<input type="text" id="name" name="name" value="<?php echo $story['name']; ?>"><br><br>
 		
 		<label for="email">Email:</label>
-		<input type="email" id="email" name="email" value="<?php echo $story['email']; ?>"><br>
+		<input type="email" id="email" name="email" value="<?php echo $story['email']; ?>"><br><br>
 		
 		<label for="title">Title:</label>
-		<input type="text" id="title" name="title" value="<?php echo $story['name']; ?>"><br>
+		<input type="text" id="title" name="title" value="<?php echo $story['title']; ?>"><br><br>
 
 		<label for="story">Story:</label>
-		<textarea id="story" name="story"><?php echo $story['story']; ?></textarea><br>
+		<textarea id="story" name="story"><?php echo $story['story']; ?></textarea><br><br>
 		
 		<label for="location">Location:</label>
-		<input type="text" id="location" name="location" value="<?php echo $story['location']; ?>"><br>
+		<input type="text" id="location" name="location" value="<?php echo $story['location']; ?>"><br><br>
 		
 		<label for="picture">Picture:</label>
-		<input type="file" id="photo" name="picture" accept="image/*"><br>
+		<input type="file" id="photo" name="picture" accept="image/*"><br><br>
 		
 		<label for="video">Video:</label>
-		<input type="file" id="video" name="video" accept="video/*"><br>
+		<input type="file" id="video" name="video" accept="video/*"><br><br>
 		
 		<input type="hidden" name="id" value="<?php echo $story['id']; ?>">
 		
 		<input type="submit" value="Save Changes">
 	</form>
+		</main>
 <footer>
     <hr>
     <div class="container">
